aa80fa7e510ad9eefb07416363588fea6edd273d
Antonio Serrano Munoz <aserranomuno@nvidia.com>
2024-07-21 09:49:48 -0400

    Unify humanoid agent configs

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/classic/humanoid/agents/rsl_rl_ppo_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/classic/humanoid/agents/rsl_rl_ppo_cfg.py
@@ -26,7 +26,7 @@ class HumanoidPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         activation="elu",
     )
     algorithm = RslRlPpoAlgorithmCfg(
-        value_loss_coef=1.0,
+        value_loss_coef=2.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
         entropy_coef=0.0,
